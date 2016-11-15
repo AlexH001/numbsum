9f7e0790848f615ce048df9c7b537551e98b8334
AlexH001 <ah685@exeter.ac.uk>
2016-11-15 13:34:37 +0000

    determing weather the sum is even or odd

--- hello.c
+++ hello.c
@@ -16,6 +16,11 @@ int main (void)
 
     // display output     
     printf("%d + %d = %d", n1, n2, Sum);
+	
+	// check if the sum is even or odd, 
+    // and print result to screen
+    if(Sum % 2 == 0) printf("\n%d is even\n", Sum);
+    else printf("\n%d is odd\n", Sum);
 
     return 0;
 }
